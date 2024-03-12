96f3c5538ba5e777dada80aa17a2dd303f5e76a6
Caio Andrade <caio.andrade2442@gmail.com>
2024-03-12 12:40:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Mark", "Wade", "Dave", "Gilbert", "Dan" };
+
+            Array.Sort(names);
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
